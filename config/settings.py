8212b66c5dbdd227b8fb4f5c67dde6f8ecf7ad04
matthew hill <matthewhill@matthews-iMac.local>
2021-03-11 21:55:04 +0000

    ch7

--- config/settings.py
+++ config/settings.py
@@ -37,14 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # new
 
     # Third-party
-    'crispy_forms', # new
+    'crispy_forms',
+    'allauth', # new
+    'allauth.account', # new
 
-    #Local
+    # Local
     'accounts',
     'pages',
 ]
+
+
+
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4' 
@@ -145,7 +151,20 @@ STATICFILES_FINDERS = [ # new
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser' # new
+ACCOUNT_SESSION_REMEMBER = True # new
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False # new
 
-LOGIN_REDIRECT_URL = 'home' # new
-LOGOUT_REDIRECT_URL = 'home' # new
+ACCOUNT_USERNAME_REQUIRED = False # new
+ACCOUNT_AUTHENTICATION_METHOD = 'email' # new
+ACCOUNT_EMAIL_REQUIRED = True # new
+ACCOUNT_UNIQUE_EMAIL = True # new
 
+
+LOGIN_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home' # new
+SITE_ID = 1
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- config/urls.py
+++ config/urls.py
@@ -22,9 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # User management
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')), # new
 
     # Local apps
-    path('accounts/', include('accounts.urls')), # new
     path('', include('pages.urls')),
 ]
